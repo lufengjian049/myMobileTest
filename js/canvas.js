eefc5df872dd6dc653ed72d867c4e7269338decd
cherishlu <lufengjian0409@163.com>
2014-11-14 18:26:03 +0800

    v3.8

--- js/canvas.js
+++ js/canvas.js
@@ -22,118 +22,35 @@ $(function(){
 //	context.beginPath();
 	//second
 	var seccanvas=$("#seccanvas"),ctx2=seccanvas[0].getContext("2d");
-	drawarc(canvas,ctx2,{radius:70,shadow:true});
-	drawpartarc(canvas,ctx2,{radius:66,startAngle:0,endAngle:Math.PI/1.5,color:"#047bc1",text:"Commit",amount:"21.1万元"});
-	drawpartarc(canvas,ctx2,{radius:60,startAngle:Math.PI/1.5,endAngle:Math.PI*1.4,color:"#fcbc13",text:"待Sell out",amount:"3.1万元"});
-	drawpartarc(canvas,ctx2,{radius:54,startAngle:Math.PI*1.4,endAngle:Math.PI*2,color:"#f35040",text:"已Sell out",amount:"0.1万元"});
+	Util.drawarc(seccanvas,ctx2,{radius:70,shadow:true});
+	Util.drawpartarc(seccanvas,ctx2,{radius:66,startAngle:0,endAngle:Math.PI/1.5,color:"#047bc1",text:"Commit",amount:"21.1万元"});
+//	Util.drawpartarc(seccanvas,ctx2,{radius:60,startAngle:Math.PI/1.5,endAngle:Math.PI*1.4,color:"#fcbc13",text:"待Sell out",amount:"3.1万元"});
+//	Util.drawpartarc(seccanvas,ctx2,{radius:54,startAngle:Math.PI*1.4,endAngle:Math.PI*2,color:"#f35040",text:"已Sell out",amount:"0.1万元"});
 	ctx2.globalCompositeOperation="destination-out";
-	drawarc(canvas,ctx2,{radius:40,shadow:false});//遮罩
+	Util.drawarc(seccanvas,ctx2,{radius:40,shadow:false});//遮罩
 	
-	var seccanvas=$("#thirdcanvas"),ctx3=seccanvas[0].getContext("2d");
-	ctx3.beginPath();
-	ctx3.arcTo(10,10,100,100,20);
-	ctx3.fillStyle="#ccc";
-	ctx3.fill();
+	var thirdcanvas=$("#thirdcanvas"),ctx3=thirdcanvas[0].getContext("2d");
+	Util.drawRect(thirdcanvas,ctx3,{color:"#f14b40",drawinfo:[{text:"审核中",amount:"322323.2"},{text:"已审未sell out",amount:"54323.45"}]});
+	
+	var forthcanvas=$("#forthcanvas"),ctx4=forthcanvas[0].getContext("2d");
+	Util.drawarc(forthcanvas,ctx4,{radius:70,shadow:true});
+//	Util.drawpartarc(forthcanvas,ctx4,{radius:66,startAngle:0,endAngle:Math.PI/1.7,color:"#2796d9",text:"审核中",amount:"4.12万元"});
+//	Util.drawpartarc(forthcanvas,ctx4,{radius:60,startAngle:Math.PI/1.7,endAngle:Math.PI/1.3,color:"#ead038",text:"已签未付款",amount:"2.24万元"});
+//	Util.drawpartarc(forthcanvas,ctx4,{radius:62,startAngle:Math.PI/1.3,endAngle:Math.PI*1.1,color:"#ee9840",text:"已签未未发货",amount:"2.01万元"});
+//	Util.drawpartarc(forthcanvas,ctx4,{radius:66,startAngle:Math.PI*1.1,endAngle:Math.PI*1.4,color:"#59a8b3",text:"签约中",amount:"1.23万元"});
+//	Util.drawpartarc(forthcanvas,ctx4,{radius:58,startAngle:Math.PI*1.4,endAngle:Math.PI*2,color:"#73964e",text:"已审未签",amount:"1.98万元"});
+	ctx4.globalCompositeOperation="destination-out";
+	Util.drawarc(forthcanvas,ctx4,{radius:40,shadow:false});//遮罩
+	
+	var fivecanvas=$("#fivecanvas"),ctx5=fivecanvas[0].getContext("2d");
+	Util.drawRect(fivecanvas,ctx5,{color:"#fcc125",drawinfo:[{text:"付款中",amount:"3456.87"},{text:"完成收款",amount:"4001.67"}]});
+	
+	var sexcanvas1=$("#sexcanvas1"),ctx6=sexcanvas1[0].getContext("2d");
+	Util.drawOrderTips(sexcanvas1,ctx6,{});
+	sexcanvas1.css("left","58%");
+	sexcanvas1.next().css("left","48%");
+	var sexcanvas2=$("#sexcanvas2"),ctx7=sexcanvas2[0].getContext("2d");
+	Util.drawOrderTips(sexcanvas2,ctx7,{graSColor:"#2499d0",graEColor:"#2489c3"});
+	sexcanvas2.css("left","28%");
+	sexcanvas2.next().css("left","18%");
 })
-function drawarc(canvas,ctx2,options){
-	options=$.extend({},{shadow:false,startAngle:0,endAngle:Math.PI*2},options);
-	ctx2.beginPath();
-	ctx2.arc(canvas.width()/2,canvas.height()/2,options.radius,options.startAngle,options.endAngle,false);
-	if(options.shadow){
-		ctx2.shadowOffsetX=0;
-		ctx2.shadowOffsetY=0;
-		ctx2.shadowBlur=4;
-		ctx2.shadowColor="#687278";
-		ctx2.strokeStyle="rgba(111,120,125,.5)";
-		ctx2.fillStyle="transparent";
-		ctx2.stroke();
-	}else{
-		ctx2.fillStyle=options.color;
-	}
-	ctx2.fill();
-	ctx2.closePath();
-}
-//画部分圆--附带 添加折线 提示功能
-function drawpartarc(canvas,ctx,options){
-	options=$.extend({},{startAngle:0,endAngle:Math.PI*2},options);
-	ctx.beginPath();
-	var x=canvas.width()/2,y=canvas.height()/2;
-	ctx.moveTo(x,y);
-//	ctx.lineTo(x+options.radius*Math.cos(options.startAngle),(options.radius*Math.sin(options.startAngle)+y));
-//	ctx.arcTo(x-options.radius,(options.radius*Math.sin(options.startAngle)+y),(x-options.radius*Math.cos(options.endAngle)),(options.radius*Math.sin(options.endAngle)+y),options.radius);
-	ctx.arc(x,y,options.radius,options.startAngle,options.endAngle,false);
-//	ctx.lineTo(x,y);
-	//ctx.strokeStyle="transparent";
-//	ctx.stroke();
-	ctx.fillStyle=options.color;
-	ctx.fill();
-	ctx.closePath();
-	options.x=x;options.y=y;
-	drawFoldLine(ctx,options);
-}
-//画折线
-function drawFoldLine(ctx,options){
-	var lineStartAngle=(options.endAngle-options.startAngle)/2 + options.startAngle,
-	lineRadius=options.radius - (options.radius-40)/2,startpos={x:'',y:''},foldPos={x:'',y:''},  //foldPos 折点坐标
-	endPos={x:'',y:''},foldx=20,foldy=20,linewidth=70,arcPos={x:'',y:''},arcradius=2,
-	textpos={x:'',y:''},textHeight=10;
-	ctx.beginPath();
-	switch (true){
-		case lineStartAngle<=Math.PI/2 :
-			startpos.x=lineRadius*Math.cos(lineStartAngle)+options.x;
-			startpos.y=lineRadius*Math.sin(lineStartAngle)+options.y;
-			foldPos.x=startpos.x+foldx;foldPos.y=startpos.y+foldy;
-			endPos.x=linewidth+foldPos.x;endPos.y=foldPos.y;
-			arcPos.x=endPos.x+arcradius;arcPos.y=endPos.y;
-			textpos.x=endPos.x-linewidth/2;textpos.y=endPos.y-textHeight;
-		break;
-		case (lineStartAngle<=Math.PI &&  lineStartAngle > Math.PI/2):
-			lineStartAngle=Math.PI-lineStartAngle;
-			startpos.x=options.x-lineRadius*Math.cos(lineStartAngle);
-			startpos.y=lineRadius*Math.sin(lineStartAngle)+options.y;
-			foldPos.x=startpos.x-foldx;foldPos.y=startpos.y+foldy;
-			endPos.x=foldPos.x-linewidth;endPos.y=foldPos.y;
-			arcPos.x=endPos.x-arcradius;arcPos.y=endPos.y;
-			textpos.x=endPos.x+linewidth/2;textpos.y=endPos.y-textHeight;
-		break;
-		case (lineStartAngle<=Math.PI*1.5 && lineStartAngle > Math.PI):
-			lineStartAngle= lineStartAngle -Math.PI;
-			startpos.x=options.x-lineRadius*Math.cos(lineStartAngle);
-			startpos.y=options.y-lineRadius*Math.sin(lineStartAngle);
-			foldPos.x=startpos.x-foldx;foldPos.y=startpos.y-foldy;
-			endPos.x=foldPos.x-linewidth;endPos.y=foldPos.y;
-			arcPos.x=endPos.x-arcradius;arcPos.y=endPos.y;
-			textpos.x=endPos.x+linewidth/2;textpos.y=endPos.y-textHeight;
-		break;
-		case (lineStartAngle<=Math.PI*2 && lineStartAngle>Math.PI * 1.5 ):
-			lineStartAngle= Math.PI*2 - lineStartAngle;
-			startpos.x=options.x+lineRadius*Math.cos(lineStartAngle);
-			startpos.y=options.y-lineRadius*Math.sin(lineStartAngle);
-			foldPos.x=startpos.x+foldx;foldPos.y=startpos.y-foldy;
-			endPos.x=linewidth+foldPos.x;endPos.y=foldPos.y;
-			arcPos.x=endPos.x+arcradius;arcPos.y=endPos.y;
-			textpos.x=endPos.x-linewidth/2;textpos.y=endPos.y-textHeight;
-		break;
-	}
-	//画折线
-	ctx.moveTo(startpos.x,startpos.y);
-	ctx.lineTo(foldPos.x,foldPos.y);
-	ctx.lineTo(endPos.x,endPos.y);
-	ctx.strokeStyle="#fff";
-	ctx.stroke();
-	ctx.closePath();
-	//结束路径 开始 画折线圆
-	ctx.beginPath();
-	ctx.arc(arcPos.x,arcPos.y,arcradius,0,Math.PI*2,false);
-	ctx.strokeStyle="#fff";
-	ctx.stroke();
-	ctx.fillStyle=options.color;
-	ctx.fill();
-	ctx.closePath();
-	//绘制文本
-	ctx.textAlign="center";
-	ctx.font="6px arial";
-	ctx.fillStyle="#000";
-	ctx.fillText(options.text,textpos.x,textpos.y);
-	ctx.fillText(options.amount,textpos.x,textpos.y+10);
-}
--- js/util.js
+++ js/util.js
@@ -1,7 +1,16 @@
 var Util={
 	os:{
 		ios:null,
-		android:null
+		android:null,
+		height:window.screen.height
+	},
+	getOs:function(){
+		var ua=navigator.userAgent,android = ua.match(/(Android);?[\s\/]+([\d.]+)?/);
+		if(android){
+			this.os.android = true;
+            this.os.version = android[2];
+            (window.devicePixelRatio) && (this.os.height=window.screen.height/window.devicePixelRatio);
+		}
 	},
 	support : {
 		storage : !!window.localStorage
@@ -94,45 +103,185 @@ var Util={
 			}
 		}
 	},
-	startMove:function(obj,json,fnEnd){
-		//如多物体运动，引出在每一个运动元素上面加一个单独的定时器，技巧--在每一个元素上面加属性
-	    clearInterval(obj.timer);
-
-	    //为元素对象，加属性-定时器
-	    obj.timer = setInterval(function () {
-	        var bStop = true; //假设，所有的值都已经到所设定的值
-	        for (var attr in json) {
-	            var iTarget = json[attr];
-
-	            //定时器没循环一次所执行的操作，
-	            //先将当前元素的当前属性的值取出来
-	            var cur = 0;
-	            //如果当前的元素属性是 透明度相关，则需要进行单独处理
-	            if (attr == "opacity") { //进行单独处理
-	                //我们进行处理的透明度 化成 整数，但是要对一些特殊的小数*100的结果进行四舍五入,如 0.07*100=7.0000000001
-	                cur = Math.round(parseFloat(getStyle(obj, attr)) * 100);
-	            } else {
-	                cur = parseInt(getStyle(obj, attr));
-	            }
-	            var speed = (iTarget - cur) / 6;
-	            //在距离目标很近的时候，speed 会是一个小数值，而小数px 在css中会默认舍去，则元素不会再继续前进，如果每次都向上取整一次，元素会1像素1像素的前进，直到终点
-	            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed); //进行取整为了 能够到达正确的位置
-
-	            if (iTarget != cur)
-	                bStop = false;
-
-	            if (attr == "opacity") {//对 opacity 透明度进行特殊处理
-	                obj.style.filter = 'alpha(opacity:' + (cur + speed) + ')';
-	                obj.style.opacity = (cur + speed) / 100;
-	            } else {
-	                obj.style[attr] = cur + speed + "px";
-	            }
-	        }
-	        if (bStop) {
-	            clearInterval(obj.timer);
-	            if (fnEnd) fnEnd();
-	        }
-	    }, 30);
+	drawarc:function(canvas,ctx2,options){//画圆
+		options=$.extend({},{x:canvas.width()/2,y:canvas.height()/2,shadow:false,startAngle:0,endAngle:Math.PI*2,linewidth:2,strokeColor:'',color:'',radius:2},options);
+		ctx2.beginPath();
+		ctx2.arc(options.x,options.y,options.radius,options.startAngle,options.endAngle,false);
+		if(options.shadow){
+			ctx2.shadowOffsetX=0;
+			ctx2.shadowOffsetY=0;
+			ctx2.shadowBlur=4;
+			ctx2.shadowColor="#687278";
+			ctx2.strokeStyle="rgba(111,120,125,.5)";
+			ctx2.fillStyle="transparent";
+			ctx2.stroke();
+		}else{
+			ctx2.fillStyle=options.color;
+		}
+		if(options.strokeColor){
+			ctx2.strokeStyle=options.strokeColor;
+			ctx2.lineWidth=options.linewidth;
+			ctx2.stroke();
+		}
+		ctx2.fill();
+		ctx2.closePath();
+	},
+	drawpartarc:function(canvas,ctx,options){ //画部分圆--附带 添加折线 提示功能
+		options=$.extend({},{startAngle:0,endAngle:Math.PI*2},options);
+		ctx.beginPath();
+		var x=canvas.width()/2,y=canvas.height()/2;
+		ctx.moveTo(x,y);
+		this.canvasMove({start:options.startAngle,end:options.endAngle},function(start,end){
+			ctx.arc(x,y,options.radius,start,end,false);
+			ctx.fillStyle=options.color;
+			ctx.fill();
+		})
+//		ctx.arc(x,y,options.radius,options.startAngle,options.endAngle,false);
+//		ctx.fillStyle=options.color;
+//		ctx.fill();
+		ctx.closePath();
+		options.x=x;options.y=y;
+		this.drawFoldLine(canvas,ctx,options);
+	},
+	drawFoldLine:function(canvas,ctx,options){//画折线
+		var lineStartAngle=(options.endAngle-options.startAngle)/2 + options.startAngle,
+		lineRadius=options.radius - (options.radius-40)/2,startpos={x:'',y:''},foldPos={x:'',y:''},  //foldPos 折点坐标
+		endPos={x:'',y:''},foldx=20,foldy=20,linewidth=70,arcPos={x:'',y:''},arcradius=2,
+		textpos={x:'',y:''},textHeight=10;
+		ctx.beginPath();
+		switch (true){
+			case lineStartAngle<=Math.PI/2 :
+				startpos.x=lineRadius*Math.cos(lineStartAngle)+options.x;
+				startpos.y=lineRadius*Math.sin(lineStartAngle)+options.y;
+				foldPos.x=startpos.x+foldx;foldPos.y=startpos.y+foldy;
+				endPos.x=linewidth+foldPos.x;endPos.y=foldPos.y;
+				arcPos.x=endPos.x+arcradius;arcPos.y=endPos.y;
+				textpos.x=endPos.x-linewidth/2;textpos.y=endPos.y-textHeight;
+			break;
+			case (lineStartAngle<=Math.PI &&  lineStartAngle > Math.PI/2):
+				lineStartAngle=Math.PI-lineStartAngle;
+				startpos.x=options.x-lineRadius*Math.cos(lineStartAngle);
+				startpos.y=lineRadius*Math.sin(lineStartAngle)+options.y;
+				foldPos.x=startpos.x-foldx;foldPos.y=startpos.y+foldy;
+				endPos.x=foldPos.x-linewidth;endPos.y=foldPos.y;
+				arcPos.x=endPos.x-arcradius;arcPos.y=endPos.y;
+				textpos.x=endPos.x+linewidth/2;textpos.y=endPos.y-textHeight;
+			break;
+			case (lineStartAngle<=Math.PI*1.5 && lineStartAngle > Math.PI):
+				lineStartAngle= lineStartAngle -Math.PI;
+				startpos.x=options.x-lineRadius*Math.cos(lineStartAngle);
+				startpos.y=options.y-lineRadius*Math.sin(lineStartAngle);
+				foldPos.x=startpos.x-foldx;foldPos.y=startpos.y-foldy;
+				endPos.x=foldPos.x-linewidth;endPos.y=foldPos.y;
+				arcPos.x=endPos.x-arcradius;arcPos.y=endPos.y;
+				textpos.x=endPos.x+linewidth/2;textpos.y=endPos.y-textHeight;
+			break;
+			case (lineStartAngle<=Math.PI*2 && lineStartAngle>Math.PI * 1.5 ):
+				lineStartAngle= Math.PI*2 - lineStartAngle;
+				startpos.x=options.x+lineRadius*Math.cos(lineStartAngle);
+				startpos.y=options.y-lineRadius*Math.sin(lineStartAngle);
+				foldPos.x=startpos.x+foldx;foldPos.y=startpos.y-foldy;
+				endPos.x=linewidth+foldPos.x;endPos.y=foldPos.y;
+				arcPos.x=endPos.x+arcradius;arcPos.y=endPos.y;
+				textpos.x=endPos.x-linewidth/2;textpos.y=endPos.y-textHeight;
+			break;
+		}
+		//画折线
+		ctx.moveTo(startpos.x,startpos.y);
+		ctx.lineTo(foldPos.x,foldPos.y);
+		ctx.lineTo(endPos.x,endPos.y);
+		ctx.strokeStyle="#fff";
+		ctx.stroke();
+		ctx.closePath();
+		//结束路径 开始 画折线圆
+		ctx.beginPath();
+		ctx.arc(arcPos.x,arcPos.y,arcradius,0,Math.PI*2,false);
+		ctx.strokeStyle="#fff";
+		ctx.stroke();
+		ctx.fillStyle=options.color;
+		ctx.fill();
+		ctx.closePath();
+		//绘制文本
+		this.drawText(canvas,ctx,{text:[options.text,options.amount],baseX:textpos.x,baseY:textpos.y});
+	},
+	drawRect:function(canvas,ctx,options){ //绘制矩形
+		var cWidth=canvas.width(),cHeight=canvas.height(),rectX=30,rectY=cHeight/2,rectWidth=cWidth-rectX*2-10,rectHeight=10,
+		totalamount=parseFloat(options.drawinfo[0].amount)+parseFloat(options.drawinfo[1].amount),
+		secRectWidth=rectWidth*(options.drawinfo[1].amount/totalamount),//蒙版矩形的宽度
+		secRectX=(rectWidth-secRectWidth)+rectX+1,secRectY=rectY-35,secRectHeight=80;//蒙版矩形的开始 x 坐标 , +1 是因为 矩形有边框
+		ctx.fillStyle=options.color;
+		ctx.strokeStyle="#000";
+		ctx.rect(rectX,rectY,rectWidth,rectHeight);
+		ctx.stroke();
+		ctx.fill();
+		//绘制第二个蒙版矩形
+		this.drawarc(canvas,ctx,{x:secRectX-1,y:secRectY,color:options.color,radius:2,strokeColor:"#fff",linewidth:3});
+		ctx.beginPath();
+		ctx.moveTo(secRectX-1,secRectY+3);
+		ctx.lineTo(secRectX-1,secRectY+80);
+		ctx.strokeStyle="#fff";
+		ctx.lineWidth=1;
+		ctx.stroke();
+		ctx.closePath();
+		ctx.fillStyle="rgba(188,200,207,.5)";
+		ctx.fillRect(secRectX,secRectY,secRectWidth,secRectHeight);
+		//绘制文本
+		this.drawText(canvas,ctx,{text:[options.drawinfo[0].text,options.drawinfo[0].amount],baseX:secRectX,baseY: secRectY+10,basealign:'left'});
+		this.drawText(canvas,ctx,{text:[options.drawinfo[1].text,options.drawinfo[1].amount],baseX:secRectX,baseY:secRectY+60,basealign:'right'});
+	},
+	drawText:function(canvas,ctx,options){//绘制文本
+		var options=$.extend({},{textalign:"center",font:"6px arial",color:"#000",text:[],baseX:'',baseY:'',basealign:'top'},options);
+		//基线 为 fillText 的 坐标   
+		//基准点的坐标 ， 显示的位置(tap,left,right,bottom),通过context.measureText(text).width; 计算坐标
+		ctx.textAlign=options.textalign;
+		ctx.font=options.font;
+		ctx.fillStyle=options.color;
+		var textX=options.baseX,textY=options.baseY,textWidthArr=[];
+		for(var i=0;i<options.text.length;i++){
+			textWidthArr.push(ctx.measureText(options.text[i]).width);
+		}
+		var theWidthest=Math.max.apply(null,textWidthArr); //获取最大的宽度
+		if(options.baseX && options.baseY){
+			switch(options.basealign){
+				case 'left':
+					textX=options.baseX-theWidthest/2-2;
+				break;
+				case 'right':
+					textX=options.baseX+theWidthest/2+2;
+				break;
+			}
+		}
+		for(var i=0;i<options.text.length;i++){
+			ctx.fillText(options.text[i],textX,textY);
+			textY+=12;
+		}
+	},
+	drawOrderTips:function(canvas,ctx,options){ //绘制-排名的小椭圆
+		var options=$.extend({}, {graSColor:"#f83741",graEColor:"#f530a2"}, options),cwidth=28,cheight=28;
+		ctx.beginPath();
+		ctx.moveTo(cwidth/2,0);
+		ctx.quadraticCurveTo(0,0,cwidth/2,cheight);
+		ctx.moveTo(cwidth/2,0);
+		ctx.quadraticCurveTo(cwidth,0,cwidth/2,cheight);
+		var lig=ctx.createLinearGradient(cwidth/2,0,cwidth/2,cheight);
+		lig.addColorStop(0,options.graSColor);
+		lig.addColorStop(1,options.graEColor);
+		ctx.fillStyle=lig;   //"#f53540";
+		ctx.fill();
+		this.drawarc(canvas,ctx,{x:cwidth/2,y:10,radius:3,color:"#fff"});
+	},
+	canvasMove:function(pro,action){
+		//pro--start end
+		var options=$.extend({},{dur:400},pro);
+		var count=10,itemtime=options.dur/count,item=(options.end-options.start)/count;
+		var t=setInterval(function(){
+			var startpro=options.start,endpro=startpro+item;
+			action(startpro,endpro);
+			if(endpro == options.end){
+				clearInterval(t);
+			}
+			options.start=endpro;options.end=endpro+item;
+		},itemtime);
 	}
 }
 Date.prototype.format = function(format){ 
